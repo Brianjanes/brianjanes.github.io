6d9a41f96ee53154aee97584f4316a38023840c7
Brian Janes <janes.brian@gmail.com>
2023-09-15 08:46:36 -0230

    added astro for github items

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
@@ -8,12 +8,16 @@ import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://astrofy-template.netlify.app',
-  integrations: [mdx(), sitemap(), tailwind(), image(
-    {
-      serviceEntryPoint: '@astrojs/image/sharp',
+  site: "https://brianjanes.github.io",
+  // base: "/brianjanes.github.io",
+  integrations: [
+    mdx(),
+    sitemap(),
+    tailwind(),
+    image({
+      serviceEntryPoint: "@astrojs/image/sharp",
       cacheDir: "./.cache/image",
-      logLevel: 'debug',
-    }
-  )]
+      logLevel: "debug",
+    }),
+  ],
 });
